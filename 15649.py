8f2d9f01000bf2b98a008cc67e398db2dfff0f62
iioo3356 <54922639+iioo3356@users.noreply.github.com>
2021-01-14 13:23:36 +0900

    Create 15649.py
    https://www.youtube.com/watch?v=Enz2csssTCs
    강의 보고 파이썬으로 바꿔 적어보았다!! 이제야 백트래킹이 이해가 되는 기분

---
+++ 15649.py
@@ -0,0 +1,16 @@
+def go(cnt):
+    global n, m, arr, isused
+    if cnt == m:
+        print(' '.join(map(str, arr)))
+        return
+    for i in range(1, n+1):
+        if (isused[i] == False):
+            arr[cnt] = i
+            isused[i] = True
+            go(cnt+1)
+            isused[i] = False
+
+n, m = map(int, input().split())
+arr = [0]*m
+isused = [False]*(n+1)
+go(0)
