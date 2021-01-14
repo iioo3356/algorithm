4de65d56099518774667dd63b410181c6110ab8c
iioo3356 <54922639+iioo3356@users.noreply.github.com>
2021-01-14 13:48:40 +0900

    Update 15651.py

--- 15651.py
+++ 15651.py
@@ -1,17 +1,14 @@
 def go(cnt):
     global n, m, arr, isused
     if cnt == m:
-        if arr == sorted(arr):
-            print(' '.join(map(str, arr)))
-            return
-        else:
-            return
+        print(' '.join(map(str, arr)))
+        return
     for i in range(1, n+1):
         if (isused[i] == False):
             arr[cnt] = i
-            isused[i] = True
+            # isused[i] = True
             go(cnt+1)
-            isused[i] = False
+            # isused[i] = False
 
 n, m = map(int, input().split())
 arr = [0]*m
