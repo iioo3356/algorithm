29e8008963a90c7306b50e0e1c4044ead46740e1
김하연 <54922639+iioo3356@users.noreply.github.com>
2022-01-10 00:27:28 +0900

    Update and rename failure_rate.CPP to failure_rate.cpp

--- failure_rate.cpp
+++ failure_rate.cpp
@@ -1,8 +1,18 @@
+// 프로그래머스 실패율 문제 풀이
+
 #include <string>
 #include <vector>
 #include <algorithm>
 #include <iostream>
 using namespace std;
+
+bool cmp(pair<int, double> a, pair<int, double> b){
+    if (a.second == b.second){
+        return a.first < b.first;
+    }
+    else{
+        return a.second > b.second;}
+}
 
 vector<int> solution(int N, vector<int> stages) {
     vector<int> answer;
@@ -11,41 +21,35 @@ vector<int> solution(int N, vector<int> stages) {
     int parentNum = stages.size();
     int childNum = 0;
     sort(stages.begin(), stages.end());
+    for (int i=stages.back()+1;i<N+1;i++){
+        memo.push_back(make_pair(i, 0.0));
+    }
     for (int i=0; i<stages.size();i++){
-        
-
         if (stages[i] == currentStage){            
             childNum += 1;
             if (i < stages.size()-1){
                 continue;  
             }
             else {
-                memo.push_back(make_pair(currentStage,(double)childNum/parentNum) );
+                memo.push_back(make_pair(currentStage,(double)childNum/parentNum));
                 break;
             }
-        }
-            memo.push_back(make_pair(currentStage, (double)childNum/parentNum));
-            parentNum -= childNum;
-            childNum = 1;
+        }        
+        memo.push_back(make_pair(currentStage, (double)childNum/parentNum));
+        parentNum -= childNum;
+        childNum = 1;
         for (int j=currentStage+1;j<stages[i];j++){
             memo.push_back(make_pair(j, 0.0));
         }
-            currentStage = stages[i];
+        currentStage = stages[i];
         if (currentStage == N+1){
             break;
         }
     }
-    cout << endl;
-    for (int i=0;i<stages.size();i++){
-     cout << stages[i] << " ";
-
-    }
-    cout << endl;
+    sort(memo.begin(), memo.end(), cmp);
     for (int i=0;i<memo.size();i++){
-        cout << memo[i].first << " " << memo[i].second << endl;
+        answer.push_back(memo[i].first);
     }
-    
-    
     
     return answer;
 }
