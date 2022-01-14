f833b1c6aec3c7a48c9db0254f8e4ed6821003ea
김하연 <54922639+iioo3356@users.noreply.github.com>
2022-01-14 23:48:50 +0900

    Create dot_product.cpp

---
+++ dot_product.cpp
@@ -0,0 +1,13 @@
+// 프로그래머스 내적
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> a, vector<int> b) {
+    int answer = 0;
+    for (int i=0;i<a.size();i++){
+        answer += (a[i]*b[i]);
+    }
+    return answer;
+}
