92b325d3ebf46cf2823042d33749d9473abbee8d
김하연 <54922639+iioo3356@users.noreply.github.com>
2022-01-20 23:55:23 +0900

    Update crain.cpp

--- crain.cpp
+++ crain.cpp
@@ -1,3 +1,4 @@
+// 프로그래머스 크레인 인형뽑기
 #include <string>
 #include <vector>
 #include <stack>
@@ -6,26 +7,20 @@ using namespace std;
 
 int solution(vector<vector<int>> board, vector<int> moves) {
     int answer = 0;
-    stack<int> basket ;
+    stack<int> basket;
     basket.push(-1);
     for (int i=0;i<moves.size();i++){
-        // cout << moves[i] << " move" << endl;
         for (int j=0;j<board.size();j++){
             if (board[j][moves[i]-1] > 0){
-                // cout << board[j][moves[i]-1]<< endl;
-                // cout << "top " << basket.top() << endl;
+                cout << board[j][moves[i]-1]<< endl;
                 if (basket.top() == board[j][moves[i]-1]){
-                    basket.pop();
                     basket.pop();
                     answer += 2;
                 }
                 else{
                     basket.push(board[j][moves[i]-1]);
-
                 }
                 board[j][moves[i]-1] = 0;
-
-                // cout << board[j][moves[i]-1] << endl;
                 break;
             }
         }
