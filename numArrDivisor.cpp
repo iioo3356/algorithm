fdc1f5a12050ffd9b9bbcd60361772f9016d1933
김하연 <54922639+iioo3356@users.noreply.github.com>
2022-01-31 23:42:54 +0900

    Create numArrDivisor.cpp

---
+++ numArrDivisor.cpp
@@ -0,0 +1,21 @@
+// 나누어 떨어지는 숫자 배열
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor) {
+    vector<int> answer;
+    for (int i=0;i<arr.size();i++){
+        if (arr[i]%divisor==0){
+          answer.push_back(arr[i]);  
+        }
+    }
+    if (answer.size() == 0){
+        answer.push_back(-1);
+    }
+    else {
+        sort(answer.begin(), answer.end());
+    }
+    return answer;
+}
