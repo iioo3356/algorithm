3b92ada70919f07e3781cc090072f0be314db404
김하연 <54922639+iioo3356@users.noreply.github.com>
2022-05-14 23:38:32 +0900

    Create 25804.cpp

---
+++ 25804.cpp
@@ -0,0 +1,13 @@
+// boj 25803 새싹
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "         ,r\'\"7" << endl;
+    cout << "r`-_   ,\'  ,/" << endl;
+    cout << " \\. \". L_r\'" << endl;
+    cout << "   `~\\/" << endl;
+    cout << "      |"<< endl;
+    cout << "      |" << endl; 
+    return 0;
+}
