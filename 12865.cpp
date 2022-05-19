b6da942288a5685956677c68c39a64d5ce38e2f6
김하연 <54922639+iioo3356@users.noreply.github.com>
2022-05-20 00:07:01 +0900

    Update 12865.cpp

--- 12865.cpp
+++ 12865.cpp
@@ -4,6 +4,9 @@
 #include <algorithm>
 using namespace std;
 
+
+int dp[101][100001];
+
 bool cmp(pair<int,int> a, pair<int,int> b){
     if (a.second == b.second ) return a.first < b.first;
     return a.second > b.second;
@@ -16,13 +19,12 @@ int main(){
     for (int i=0;i<n;i++){
         cin >> w >> v;
         items.push_back(make_pair(w, v));
-    }
-    int dp[n+1][k+1]; 
+    } 
     for(int i=1;i<=n;i++){
-        int weight = items[i].first;
-        int value=items[i].second;
+        int weight = items[i-1].first;
+        int value=items[i-1].second;
         for(int j=0;j<=k;j++){
-            if (j < items[i].first) dp[i][j]=dp[i-1][j];
+            if (j < items[i-1].first) dp[i][j]=dp[i-1][j];
             else dp[i][j] = max(dp[i-1][j],dp[i-1][j-weight]+value);
         }
     } 
