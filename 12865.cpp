cd72c58556709fa2189a04b784b89e54e22c0a87
김하연 <54922639+iioo3356@users.noreply.github.com>
2022-05-19 23:59:51 +0900

    Create 12865.cpp

---
+++ 12865.cpp
@@ -0,0 +1,31 @@
+//boj 12865 평범한 배낭
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool cmp(pair<int,int> a, pair<int,int> b){
+    if (a.second == b.second ) return a.first < b.first;
+    return a.second > b.second;
+}
+
+int main(){
+    int n, k, w, v;
+    vector<pair<int,int>> items;
+    cin >> n >> k;
+    for (int i=0;i<n;i++){
+        cin >> w >> v;
+        items.push_back(make_pair(w, v));
+    }
+    int dp[n+1][k+1]; 
+    for(int i=1;i<=n;i++){
+        int weight = items[i].first;
+        int value=items[i].second;
+        for(int j=0;j<=k;j++){
+            if (j < items[i].first) dp[i][j]=dp[i-1][j];
+            else dp[i][j] = max(dp[i-1][j],dp[i-1][j-weight]+value);
+        }
+    } 
+    cout << dp[n][k];
+    return 0;
+}
