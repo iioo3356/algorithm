1a5e5d45c4a8a6cc0a1f76aa027dffbe5c0ae789
김하연 <54922639+iioo3356@users.noreply.github.com>
2022-05-27 00:05:09 +0900

    Update 14501.cpp

--- 14501.cpp
+++ 14501.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main() {
     int n, t, p;
     cin >> n;
-    vector<int> dp(n+1,0);
+    vector<int> dp(18,0);
     vector<pair<int,int>> v;
     v.push_back(make_pair(0,0));
     for (int i=0;i<n;i++){
@@ -16,11 +16,11 @@ int main() {
     }
     int d;
     for (int i=n;i>0;i--){
-        if (v[i].first > n=i+1){
+        if (v[i].first > n-i+1){
             dp[i] = dp[i+1];
         }
         else {
-            dp[i] = max(dp[i+1], dp[d]+v[i].second);
+            dp[i] = max(dp[i+1], dp[i+v[i].first]+v[i].second);
         }
     }
     cout << dp[1] << endl;
