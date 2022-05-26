aa9125d918047661ae4fb4e0462fc7e359dfe966
김하연 <54922639+iioo3356@users.noreply.github.com>
2022-05-26 23:59:26 +0900

    Create 14501.cpp

---
+++ 14501.cpp
@@ -0,0 +1,28 @@
+// boj 14501 퇴사
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, t, p;
+    cin >> n;
+    vector<int> dp(n+1,0);
+    vector<pair<int,int>> v;
+    v.push_back(make_pair(0,0));
+    for (int i=0;i<n;i++){
+        cin >> t >> p;
+        v.push_back(make_pair(t, p));
+    }
+    int d;
+    for (int i=n;i>0;i--){
+        if (v[i].first > n=i+1){
+            dp[i] = dp[i+1];
+        }
+        else {
+            dp[i] = max(dp[i+1], dp[d]+v[i].second);
+        }
+    }
+    cout << dp[1] << endl;
+    return 0;
+}
