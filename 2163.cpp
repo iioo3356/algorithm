1e99eb8ea3c3e0d93caf4b688b1bbdb97a473efc
김하연 <54922639+iioo3356@users.noreply.github.com>
2022-05-27 23:55:32 +0900

    Create 2163.cpp

---
+++ 2163.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector<pair<int,int>> v;
+    int answer = 0;
+    int n, m;
+    cin >>n >> m;
+    cout << n*m -1 << endl;
+}
