c634a1e13769213c196fd1a9209ae5364a9b0de9
김하연 <54922639+iioo3356@users.noreply.github.com>
2023-01-22 00:34:47 +0900

    Create ThreeMusketeers.js

---
+++ ThreeMusketeers.js
@@ -0,0 +1,12 @@
+// 프로그래머스 스쿨 삼총사
+function solution(number) {
+    var answer = 0;
+    for (let i=0;i<number.length;i++){
+        for (let j=i+1;j<number.length;j++){
+            for (let k=j+1;k<number.length;k++){
+                if (number[i]+number[j]+number[k]=== 0) answer++;
+            }
+        }
+    }
+    return answer;
+}
