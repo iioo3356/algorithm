9e592ac1288473f719bed8d96059cd0bee1974ab
김하연 <54922639+iioo3356@users.noreply.github.com>
2023-02-17 23:40:16 +0900

    Update sumOfConsecutiveSequence.js

--- sumOfConsecutiveSequence.js
+++ sumOfConsecutiveSequence.js
@@ -2,12 +2,18 @@
 function solution(elements) {
     var answer = 0;
     const sumSet = new Set();
-    for (let i=1;i<=elements.length;i++){
+    for (let i=1;i<=elements.length;i++){            
+        let sum = 0;
         for (let j=0;j<elements.length;j++){
-            let sum = 0;
-            for (let k=j;k-j<i;k++){
-                sum += elements[k%elements.length]
-            }        
+            if (j === 0) {
+                for (let k=0;k<i;k++) {
+                    sum += elements[k];
+                }
+            }
+            else {
+                sum -= elements[j-1];
+                sum += elements[(j+i-1)%elements.length];
+            }
             sumSet.add(sum)
         }
     }
