720e278091de7e867b2aef0ed94f0ffef80897c5
김하연 <54922639+iioo3356@users.noreply.github.com>
2023-05-06 23:17:21 +0900

    Create leftRight.js

---
+++ leftRight.js
@@ -0,0 +1,14 @@
+// 프로그래머스 왼쪽 오른쪽
+function solution(str_list) {
+    var answer = [];
+    let lIdx = str_list.indexOf("l");
+    let rIdx = str_list.indexOf("r");
+
+    if (rIdx === -1 || lIdx < rIdx) {
+        answer = str_list.slice(0, lIdx);
+    } 
+    else if (lIdx === -1 || lIdx > rIdx) {
+        answer = str_list.slice(rIdx+1);
+    }
+    return answer;
+}
