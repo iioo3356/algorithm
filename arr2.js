daf0be37bed81c1b8dbf544dd518fdf93195f599
김하연 <54922639+iioo3356@users.noreply.github.com>
2023-07-27 00:05:03 +0900

    Update arr2.js

--- arr2.js
+++ arr2.js
@@ -1,10 +1,10 @@
 function solution(l, r) {
-    const answer = []
-    for (let i = l ; i <= r ; i ++) {
+    const answer = [];
+    for (let i=l;i<=r;i++) {
         if (i%5 !== 0) continue;
-        const charI = String(i)
-        if (![...charI].every(a => a === '5' || a === '0')) continue
-        answer.push(i)
+        const iStr = i.toString();
+        if ([...iStr].every(a => a === '5' || a === '0')) 
+            answer.push(i)
     }
     if (answer.length === 0) 
         answer.push(-1);
