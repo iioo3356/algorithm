209414d6841758df799c25511ac49797b3f3af15
김하연 <54922639+iioo3356@users.noreply.github.com>
2023-08-15 00:04:21 +0900

    Update randomK.js

--- randomK.js
+++ randomK.js
@@ -2,10 +2,7 @@ function solution(arr, k) {
     var answer = [];
     arr = arr.sort()
     for (let i=0;i<arr.length;i++){
-        if (i==0) answer.push(arr[i]);
-        else {
-            if (arr[i-1] !== arr[i]) answer.push(arr[i]);
-        }
+        if (!answer.includes(arr[i])) answer.push(arr[i]);
     }
     if (answer.length > k) {
         answer = answer.slice(0, k);
