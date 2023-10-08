dc94d07ec82176bee156ecb62002cf3ce981accb
김하연 <54922639+iioo3356@users.noreply.github.com>
2023-10-09 00:08:53 +0900

    Update conditionString.cpp

--- conditionString.cpp
+++ conditionString.cpp
@@ -1,13 +1,13 @@
-// 조건 문자열
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
+// 프로그래머스 조건 문자열
+#include <string>
+#include <vector>
 
-// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
-int solution(const char* ineq, const char* eq, int n, int m) {
-    int answer = 0;
-    if (ineq == '>'){
-        if (eq == '='){            
+using namespace std;
+
+int solution(string ineq, string eq, int n, int m) {
+ int answer = 0;
+    if (ineq == ">"){
+        if (eq == "="){            
             if (n >= m)
                 answer = 1; 
             else 
@@ -19,7 +19,7 @@ int solution(const char* ineq, const char* eq, int n, int m) {
         }
     }
     else {
-        if (eq == '=') {
+        if (eq == "=") {
             if (n<=m) answer = 1;
             else answer = 0;
         }
@@ -27,7 +27,7 @@ int solution(const char* ineq, const char* eq, int n, int m) {
             if (n<m) answer = 1;
             else answer = 0;
         }
-            
-    }
+
+    }    
     return answer;
 }
