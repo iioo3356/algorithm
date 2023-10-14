63280e74fa5521c6636e33ea9f063e4adec32205
김하연 <54922639+iioo3356@users.noreply.github.com>
2023-10-15 00:00:07 +0900

    Create makeArr4.cpp

---
+++ makeArr4.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> stk;
+    int i=0;
+    while (i<arr.size()) {
+        if (!stk.empty() && stk[stk.size()-1] >= arr[i]) {
+            stk.pop_back();
+        }
+        if (stk.empty()){
+            stk.push_back(arr[i]);
+            i++;
+        }
+        else {
+            if (stk[stk.size()-1] < arr[i]) {
+                stk.push_back(arr[i]);
+                i++;
+            }
+        }
+    }
+    return stk;
+}
