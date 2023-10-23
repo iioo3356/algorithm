fc280df0147f7000ca70e5df81dc308f52f44192
김하연 <54922639+iioo3356@users.noreply.github.com>
2023-10-23 23:58:37 +0900

    Create network.py

---
+++ network.py
@@ -0,0 +1,15 @@
+def solution(n, computers):
+    answer = 0
+    visited = [False for i in range(n)]
+    for cur in range(n):
+        if visited[cur] == False:
+            dfs(computers, n, cur, visited)
+            answer+=1
+    return answer
+
+def dfs(computers, n, cur, visited):
+    visited[cur] == True
+    for i in range(n):
+        if i != cur and computers[cur][i] == 1:
+            if visited == False:
+                dfs(coumputers, n, i, visited)
