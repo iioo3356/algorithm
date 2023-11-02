918220e92356c326a53c01b92d581c08033334aa
김하연 <54922639+iioo3356@users.noreply.github.com>
2023-11-02 23:59:51 +0900

    Create transformation.py

---
+++ transformation.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+def solution(begin, target, words):
+    answer = 0
+    
+    if target not in words:
+        return 0
+    else:
+        q = deque()
+        q.append([begin, 0])
+        
+        while q:
+            item = q.popleft()
+            cWord = item[0]
+            cCount = item[1]
+            if cWord == target:
+                return cCount
+            
+            for word in words:            
+                count = 0
+                for i in range(len(word)):
+                    if (word[i] != cWord[i]):
+                        count+=1a
+                        
+                if (count == 1):
+                    q.append([word, cCount+1])
+
+    return answer
