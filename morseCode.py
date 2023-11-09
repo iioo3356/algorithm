567fbd739fe80e4aa85eda122abb4a87a7e50ed3
김하연 <54922639+iioo3356@users.noreply.github.com>
2023-11-09 23:50:07 +0900

    Create morseCode.py

---
+++ morseCode.py
@@ -0,0 +1,13 @@
+def solution(letter):
+    answer = ''
+    morseCode = {
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'    
+    }
+    letterList = letter.split(" ")
+    for l in letterList:
+        answer += morseCode[l]
+    return answer
