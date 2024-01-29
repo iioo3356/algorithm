24f7841a1f5ddf663d19e93b6df51ed862ea39fe
김하연 <54922639+iioo3356@users.noreply.github.com>
2024-01-29 23:57:51 +0900

    Create bandage.py

---
+++ bandage.py
@@ -0,0 +1,25 @@
+def solution(bandage, health, attacks):
+    answer = health
+    maxHealth = health
+    nextAttackIdx = 0
+    now = 0
+    combo = 1
+    while (nextAttackIdx < len(attacks) and health > 0):
+        # print("now", now)
+        if (now == attacks[nextAttackIdx][0]):
+            answer-=attacks[nextAttackIdx][1]
+            nextAttackIdx += 1
+            combo = 1
+        else:
+            if (combo == bandage[0]):
+                answer = min(maxHealth, answer + bandage[2] + bandage[1]) 
+                combo = 1
+            else:
+                answer = min(maxHealth, answer + bandage[1])
+                combo += 1
+        now += 1           
+        # print("answer", answer)
+
+    if (answer <= 0):
+        return -1
+    return answer
